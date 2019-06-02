e1eb1f146da1ff5f961607c14ab227a039929f17
Victor <victor.ossa@hotmail.com>
2019-06-02 11:22:49 -0500

    Agregar archivos de proyecto.

---
+++ GCP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GCP.Startup))]
+namespace GCP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
