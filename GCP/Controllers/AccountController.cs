4e33d72c2dfedd031fcca2f2969f1a871915f667
Victor <victor.ossa@hotmail.com>
2019-06-02 11:28:44 -0500

    Inicio del Repositorio

--- GCP/Controllers/AccountController.cs
+++ GCP/Controllers/AccountController.cs
@@ -15,6 +15,8 @@ namespace GCP.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+
+
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
